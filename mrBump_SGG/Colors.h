60963ce834756f7b0f4995510b6da324632776ae
Kiên Trần Trung <coi12tt34@gmail.com>
2021-09-24 15:57:28 +0700

    fix wrong rgb values

--- mrBump_SGG/Colors.h
+++ mrBump_SGG/Colors.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "ImGui/imgui.h"
-#include <D3D.h>
 
 #ifndef _COLORS_H_
 #define _COLORS_H_
@@ -12,9 +11,6 @@
 #define DARKERGRAY(alpha)    IM_COL32(128, 128, 128, alpha)
 #define GRAY(alpha)          IM_COL32(128, 128, 128, alpha)
 
-#define RED(alpha)           IM_COL32(255, 0, 0, alpha)
-#define DARKRED(alpha)		 IM_COL32(139, 0, 0, alpha)
-
 #define GREEN(alpha)         IM_COL32(0, 255, 0, alpha)
 #define LIMEGREEN(alpha)	 IM_COL32(33, 100, 33, alpha)
 #define LAWNGREEN(alpha)     IM_COL32(49, 99, 0, alpha)
@@ -23,13 +19,16 @@
 #define BLUE(alpha)          IM_COL32(0, 0, 255, alpha)
 #define DEEPSKYBLUE(alpha)   IM_COL32(0, 191, 25, alpha)
 #define SKYBLUE(alpha)       IM_COL32(0, 122, 204, alpha)
+#define LIGHTCYAN(alpha)	 IM_COL32(33, 100, 10, alpha)
+#define CYAN(alpha)          IM_COL32(0, 255, 255, alpha)
 
 #define YELLOW(alpha)        IM_COL32(255, 255, 0, alpha)
 #define ORANGE(alpha)        IM_COL32(255, 165, 0, alpha)
 #define DARKORANGE(alpha)    IM_COL32(100, 55, 0, alpha)
+#define RED(alpha)           IM_COL32(255, 0, 0, alpha)
+#define DARKRED(alpha)		 IM_COL32(139, 0, 0, alpha)
+
 #define PURPLE(alpha)        IM_COL32(125, 0, 25, alpha)
-#define LIGHTCYAN(alpha)	 IM_COL32(33, 100, 10, alpha)
-#define CYAN(alpha)          IM_COL32(0, 255, 255, alpha)
 #define PINK(alpha)          IM_COL32(255, 20, 14, alpha)
 
 #endif
